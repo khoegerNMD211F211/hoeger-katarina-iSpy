959668fb84004d23f6b04f634b357bf7cacf5cb7
khoegerNMD211F211 <94625957+khoegerNMD211F211@users.noreply.github.com>
2021-11-18 10:28:02 -0500

    led on off 1

---
+++ ledOnOff/ledOnOff.ino
@@ -0,0 +1,14 @@
+#define LED 7
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode( LED , OUTPUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite( LED , HIGH );   // turn on LED
+  delay(50);                    // wait half a second
+  digitalWrite( LED , LOW );    // turn off LED
+  delay(50);                    // wait half a second
+}
